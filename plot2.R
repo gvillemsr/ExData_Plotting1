e8491b5cd399600736e19a0b0a9646386fe7a123
msravi <ravim@ecu.edu>
2014-06-08 23:33:23 -0400

    Final version
    Finished the assignment

--- plot2.R
+++ plot2.R
@@ -1,6 +1,13 @@
 eleccons<-read.table("household_power_consumption.txt", 
                      header=TRUE, sep=";",skip=66636, nrows=2880)
 summary(eleccons)
-plot(eleccons[,2], eleccons[,3], main = NULL, ylab="Global Active Power")
-dev.copy(png, file = "plot1.png")
+tit<-read.table("household_power_consumption.txt", header=TRUE, sep=";", nrows=1)
+get.names<-names(tit)
+names(eleccons)<-get.names
+summary(eleccons)
+eleccons$t <- strptime(paste(eleccons$Date, eleccons$Time), "%d/%m/%Y %H:%M:%S")
+summary(eleccons)
+plot(eleccons[,10], eleccons[,3], 
+     main = NULL, xlab="", ylab="Global Active Power (kilowatts)", type="l")
+dev.copy(png, file = "plot2.png")
 dev.off()
